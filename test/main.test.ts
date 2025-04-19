a4a6c1337e00f0d51a3ec6277903851936cfe693
Laycon Gomes <laycon.gomes@tld.com.br>
2025-04-19 18:18:41 -0300

    test: update expected results

--- test/main.test.ts
+++ test/main.test.ts
@@ -11,5 +11,9 @@ test("Deve criar ordens de venda de ativo", async () => {
     await axios.post("http://localhost:3000/orders", inputOrder);
     const responseGetOrders = await axios.get("http://localhost:3000/assets/USDC/orders");
     const outputGetOrders = responseGetOrders.data;
-    expect(outputGetOrders).toBe(expect.arrayContaining(expect.anything()))
+    expect(outputGetOrders[0].asset_code).toBe("USDC");
+    expect(outputGetOrders[0].type).toBe("sell");
+    expect(outputGetOrders[0].quantity).toBe(1000);
+    expect(outputGetOrders[0].price).toBe(5.50);
+    expect(outputGetOrders[0].owner).toBe("a");    
 });
