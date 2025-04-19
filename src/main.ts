e9fb0bdc468c1060a089d0734bdaeb7a607bb757
Laycon Gomes <laycon.gomes@tld.com.br>
2025-04-19 19:15:34 -0300

    fix: update case to buy and sell

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,35 @@ const connection = pgp()("postgres://postgres:123456@localhost:5432/app")
 
 app.post("/orders",  async (req: Request, res: Response) => {
     const order_id = crypto.randomUUID()
-    const order = req.body
-    await connection.query('INSERT INTO order_book.order (order_id, asset_code, type, quantity, price, owner) VALUES ($1, $2, $3, $4, $5, $6)', [order_id, order.asset_code, order.type, order.quantity, order.price, order.owner])
+    const orderToExecute = req.body;
+    if(orderToExecute.type === "sell") {
+        const orders = await connection.query("SELECT * from order_book.order WHERE asset_code = $1 and type = $2", [orderToExecute.asset_code, "buy"]);
+        let match = false;
+        for (const order of orders) {
+            if(orderToExecute.price === parseFloat(order.price)) {
+                match = true;
+                await connection.query("DELETE order_book.order WHERE order_id = $1", [order.order_id])
+            } 
+        }
+        if(!match) {
+            await connection.query('INSERT INTO order_book.order (order_id, asset_code, type, quantity, price, owner) VALUES ($1, $2, $3, $4, $5, $6)', [order_id, orderToExecute.asset_code, orderToExecute.type, orderToExecute.quantity, orderToExecute.price, orderToExecute.owner])
+        }
+
+    }
+    if(orderToExecute.type === "buy") {
+        const orders = await connection.query("SELECT * from order_book.order WHERE asset_code = $1 and type = $2", [orderToExecute.asset_code, "sell"]);
+        let match = false;
+        for (const order of orders) {
+            if(orderToExecute.price === parseFloat(order.price)) {
+                match = true;
+                await connection.query("DELETE order_book.order WHERE order_id = $1", [order.order_id])
+            } 
+            
+        }
+        if(!match) {
+            await connection.query('INSERT INTO order_book.order (order_id, asset_code, type, quantity, price, owner) VALUES ($1, $2, $3, $4, $5, $6)', [order_id, orderToExecute.asset_code, orderToExecute.type, orderToExecute.quantity, orderToExecute.price, orderToExecute.owner])
+        }
+    }
     res.end();
 })
 
